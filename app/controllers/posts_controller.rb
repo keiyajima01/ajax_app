4b7c48254f2c791665b7577a38364226c53c939c
keiko-y <crystal0829kei@gmail.com>
2021-02-17 17:33:33 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,15 @@
 class PostsController < ApplicationController
 
-  def index  # indexアクションを定義した
-    @posts = Post.all # すべてのレコードを@postsに代入
+  def index
+    @posts = Post.all.order(id: "DESC") # 降順で表示
   end
 
-  def new
-  end
+  # 削除
+  # def new
+  # end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
 end
